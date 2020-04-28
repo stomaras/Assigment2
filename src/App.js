68a435aec9939c987a993bbc3bbe012b1c5c2707
stomaras <sptoma724@gmail.com>
2020-04-28 22:50:47 +0300

    Add Functionalities for the assigmnet2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <ol>
-            <li>Create an input field (in App Component) with a change listener which outputs the length of the entered text below for example in a paragraph <li>
+            <li>Create an input field (in App Component) with a change listener which outputs the length of the entered text below for example in a paragraph </li>
+            <li>Create a new component(=>ValidationComponent)which receives the text length as a prop</li>
+            <li>Inside the ValidationComponent, either output "Text too short" or "Text Long enough" depending on the text length(for example take 5 as minimum length)</li>
         
         </ol>
       </div>
