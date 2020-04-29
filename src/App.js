4dfb120d7db6ca8d528394d6445a6be1ebb0f96a
stomaras <sptoma724@gmail.com>
2020-04-29 11:46:03 +0300

    FIrst Task Completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+      userInput: ''
+  }
+
+  inputChangedHandler = (event) => {
+      this.setState({userInput: event.target.value});
+  }
+    
+    
+    
   render() {
     return (
       <div className="App">
-        <ol>
-            <li>Create an input field (in App Component) with a change listener which outputs the length of the entered text below for example in a paragraph </li>
-            <li>Create a new component(=>ValidationComponent)which receives the text length as a prop</li>
-            <li>Inside the ValidationComponent, either output "Text too short" or "Text Long enough" depending on the text length(for example take 5 as minimum length)</li>
         
-        </ol>
+        <input 
+            type="text" 
+            onChange={this.inputChangedHandler} 
+            value={this.state.userInput} />
+        <p>{this.state.userInput}</p>
       </div>
     );
   }
